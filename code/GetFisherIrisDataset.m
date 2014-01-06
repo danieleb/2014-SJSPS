6728558f8f59bbb98feef01f466d3a5abe666802
danieleb <danielebarchiesi@me.com>
2014-01-06 13:32:46 +0000

    added functions for reproducing mlsp figure

--- code/GetFisherIrisDataset.m
+++ code/GetFisherIrisDataset.m
@@ -1,8 +1,9 @@
-function [fea,cat] = GetFisherIris()
+function [fea,cat] = GetFisherIrisDataset()
 % Gets the fisher iris dataset from the web and returns the following variables
 % - fea: matrix containing sepal length, width, petal length and width along the columns
 % - cat: cell containing the classes
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data';
 s = urlread(url);
-[seplen,sepwid,petlen,petwid,cat] = strread(s,"%f,%f,%f,%f,%s");
-fea = [seplen, sepwid, petlen, petwid];
+C = textscan(s,'%f,%f,%f,%f,%s');
+fea = [C{1} C{2} C{3} C{4}];
+cat = C{5};
